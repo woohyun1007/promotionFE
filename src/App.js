9843e49e5109c8b3d1638f1de1e9ccd8f8f24b38
Ugaebi <52709794+Ugaebi@users.noreply.github.com>
2024-10-04 17:23:05 +0900

    first test

--- src/App.js
+++ src/App.js
@@ -7,14 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+       아
+        </p>
       </header>
     </div>
   );
